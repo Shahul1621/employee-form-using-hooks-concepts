09c1d5f3e9fba3cda9dfa4d74923e27c36fe5d2d
Shahul1621 <shahulmeeranu@gmail.com>
2024-08-21 22:46:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ function FormFeild({lableName= "Name", placeHol="Name" , req = "Name" , type = "text"}){
+  const [val, setVal] = useState(" ");
+   function updateVal(value){
+    setVal(()=>value);
+   }
+
+  return(
+    <div>
+       <label>{lableName}</label>
+      <input type={type} placeholder={"Enter your "+ placeHol} value={val} onChange={(e)=>updateVal(e.target.value)} />
+      {val === "" && <div>{req} is Required</div>}
     </div>
-  );
+  )
 }
 
-export default App;
+ function SelectionFeild( {lableName="Name", options=[]}){
+  return(
+    <div className="selectionFeild">
+      <label for="cars">{lableName}:</label>
+       <select id="selection">
+            {options.map((item)=> <option key={item} value={item}>{item}</option>) }
+       </select>
+    </div>
+  )
+}
+
+export default function App(){
+  return(
+    <div>
+      <h1>Employee</h1>
+         <FormFeild lableName="First Name" placeHol = "First Name" req = "First Name" />
+         <FormFeild lableName="Middle Name" placeHol = "Middle Name" req = "Middle Name" />
+         <FormFeild lableName="Last Name" placeHol = "Last Name" req = "Last Name" />
+         <FormFeild lableName="Birth Date" placeHol = "DOB" req = "Date" type="date" />
+         <FormFeild lableName="E-Mail" placeHol = "email" req = "Email" />
+         <FormFeild lableName="Phone Number" placeHol = "phone number" req = "Phone Number" />
+         <SelectionFeild lableName="Select Gender" options={["Selct","Male","Femal","Others"]}/>
+         <FormFeild lableName="Start Time" placeHol = '' req = "Start time" type="time" />
+         <FormFeild lableName="End Time" placeHol = '' req = "End time" type="time" />
+         <FormFeild lableName="Job Position" placeHol = 'job position' req = "Job Position"  />
+         <SelectionFeild lableName="Select Teams" options={["A","B","C"]}/>
+         <SelectionFeild lableName="Designations" options={["Select","Student","Staff","Administrator", "Office-Worker"]}/>
+         <FormFeild lableName="Billable Hours" placeHol = 'billable hours' req = "Billable Hours"  />
+         <button>submit</button>
+    </div>
+  )
+}
